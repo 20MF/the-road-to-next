3990763e35b1e9cd40b095ba9a478c8ec202de64
jasontan <j000102@sohu.com>
2026-03-01 22:39:52 +0800

    feat: 断言

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,12 +3,19 @@ import {initialTickets} from "@/data";
 import Link from "next/link";
 import {ticketPath} from "@/paths";
 
+const TICKET_ICONS={
+  OPEN:"O",
+  DONE:"X",
+  IN_PROGRESS:">"
+}
+
 const TicketsPage = () => {
   return (
     <div>
       {
         initialTickets.map((ticket) => (
           <div key={ticket.id}>
+            <div>{TICKET_ICONS[ticket.status]}</div>
             <h2 className="text-lg">{ticket.title}</h2>
             <Link href={ticketPath(ticket.id)}>view</Link>
           </div>
--- src/data.ts
+++ src/data.ts
@@ -1,14 +1,20 @@
-export const initialTickets =[
-    {
-        id:"1",
-        title:'ticket 1',
-        content:'this is the first ticket',
-        status: 'DONE'
-    },
-    {
-        id:"2",
-        title:'ticket 2',
-        content:'this is the second ticket',
-        status: 'OPEN'
-    }
+export const initialTickets = [
+  {
+    id: "1",
+    title: 'ticket 1',
+    content: 'this is the first ticket',
+    status: 'DONE' as const
+  },
+  {
+    id: "2",
+    title: 'ticket 2',
+    content: 'this is the second ticket',
+    status: 'OPEN' as const
+  },
+  {
+    id: "3",
+    title: 'ticket 3',
+    content: 'this is the third ticket',
+    status: 'IN_PROGRESS' as const
+  }
 ]
