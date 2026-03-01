939cb2522a2f2fede47bc154433b6502ed45c3dd
jasontan <j000102@sohu.com>
2026-03-01 21:42:29 +0800

    feat: 设置常量到单独文件中,方便调用

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
+import {ticketsPath} from "@/paths";
 
 export default function Home() {
-    return (
-        <div>
-            <h2 className="text-lg">Home Page</h2>
-            <Link className="underline" href='/tickets'>
-                Goto Tickets
-            </Link>
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="text-lg">Home Page</h2>
+      <Link className="underline" href={ticketsPath()}>
+        Goto Tickets
+      </Link>
+    </div>
+  )
 }
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,15 +1,16 @@
 import {allowedDisplayValues} from "next/dist/compiled/@next/font/dist/constants";
 import {initialTickets} from "@/data";
 import Link from "next/link";
+import {ticketPath} from "@/paths";
 
 const TicketsPage = () => {
   return (
     <div>
       {
         initialTickets.map((ticket) => (
-          <div>
+          <div key={ticket.id}>
             <h2 className="text-lg">{ticket.title}</h2>
-            <Link href={`/tickets/${ticket.id}`}>view</Link>
+            <Link href={ticketPath(ticket.id)}>view</Link>
           </div>
         ))
       }
