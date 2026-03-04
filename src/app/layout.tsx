6a868eb3a95ce2205dab91b6d5a1d00b713abc6e
jasontan <j000102@sohu.com>
2026-03-04 20:48:20 +0800

    feat: lucide icon使用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Link from "next/link";
 import {ticketsPath, homePath} from "@/paths";
 import {Button, buttonVariants} from "@/components/ui/button";
+import {LucideKanban} from "lucide-react";
 
 const inter = Inter({subsets: ["latin"]})
 
@@ -22,13 +23,18 @@ export default function RootLayout({
                         flex justify-between py-2.5 boarder-b">
             <div>
                 {/*两种使用按钮的方法,第一种,增加Button元素*/}
-                <Button asChild variant="outline">
-                    <Link href={homePath()}>Home</Link>
+                <Button asChild variant="ghost">
+                    <Link href={homePath()}>
+                        <LucideKanban/>
+                        <h1 className="ml-2 text-lg font-semibold">
+                            TicketBounty
+                        </h1>
+                    </Link>
                 </Button>
             </div>
             <div>
                 {/*第二种方法, 在classname中使用buttonVariants函数,把Link元素变体成Button元素*/}
-                <Link href={ticketsPath()} className={buttonVariants({variant: "outline"})}>
+                <Link href={ticketsPath()} className={buttonVariants({variant: "ghost"})}>
                     Tickets
                 </Link>
             </div>
