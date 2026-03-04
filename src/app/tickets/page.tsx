5806c9c23a1276652ea6fb7b5e594cababada242
jasontan <j000102@sohu.com>
2026-03-04 12:37:35 +0800

    feat: add Card component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,7 @@ import {initialTickets} from "@/data";
 import Link from "next/link";
 import {ticketPath} from "@/paths";
 import clsx from "clsx";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 
 const CheckIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
@@ -37,7 +38,7 @@ const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <div>
-                <h2 className="text-3xl font-bold tracking-tight">TicketsPage</h2>
+                <h2 className="text-3xl font-bold tracking-tight">Tickets</h2>
                 <p className="text-sm text-muted-foreground">
                     All your tickets at one place
                 </p>
@@ -45,20 +46,27 @@ const TicketsPage = () => {
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
                 {
                     initialTickets.map((ticket) => (
-                        <div key={ticket.id} className="w-full max-w-[420px] p-4 border rounded">
-                            <div>{TICKET_ICONS[ticket.status]}</div>
-                            <h3 className="text-lg truncate">{ticket.title}</h3>
-                            {/*clsx 能通过函数引入判断条件,修改样式*/}
-                            <p className={clsx("text-sm text-slate-500 truncate", {
-                                "line-through": ticket.status === 'DONE'
-                            })}>
-                                {ticket.content}
+                        <Card key={ticket.id} className="w-full max-w-[420px] ">
+                            <CardHeader>
+                                <CardTitle className="flex gap-x-2">
+                                    <span>{TICKET_ICONS[ticket.status]}</span>
+                                    <span className="truncate">{ticket.title}</span>
+                                </CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                {/*clsx 能通过函数引入判断条件,修改样式*/}
+                                <p className="text-sm text-slate-500 truncate">
+                                    {ticket.content}
+                                </p>
+                            </CardContent>
+                            <CardFooter>
+                                <Link href={ticketPath(ticket.id)} className="text-sm underline">
+                                    view
+                                </Link>
+                            </CardFooter>
 
-                            </p>
-                            <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                                view
-                            </Link>
-                        </div>
+
+                        </Card>
                     ))
                 }
             </div>
