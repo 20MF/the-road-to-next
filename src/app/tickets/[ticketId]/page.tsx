595b3f8ddbecf7ff498ab34b99ab1a03fc485340
jasontan <j000102@sohu.com>
2026-03-04 23:26:20 +0800

    feat: add search error

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,21 +1,37 @@
 import {initialTickets} from "@/data";
 import {allowedDisplayValues} from "next/dist/compiled/@next/font/dist/constants";
+import {Placeholder} from "@/components/placeholder";
+import Interceptors from "undici-types/interceptors";
+import retry = Interceptors.retry;
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {ticketsPath} from "@/paths";
 
 export type TicketProps = {
-  params: {
-    ticketId: string
-  }
+    params: {
+        ticketId: string
+    }
 }
 const TicketPage = ({params}: TicketProps) => {
-  const ticket = initialTickets.find(ticket => (ticket.id === params.ticketId))
+    const ticket = initialTickets.find(ticket => (ticket.id === params.ticketId))
 
-  if (!ticket) return <div>data not found!</div>
-  return (
-    <div>
-      <h2 className='text-lg'>{ticket.title}</h2>
-      <h2 className='text-sm'>{ticket.content}</h2>
-    </div>
-  )
+    if (!ticket) {
+        return (
+            <div className="flex-1 flex items-center">
+                <Placeholder label="Ticket not found" button={
+                    <Button asChild variant="outline">
+                        <Link href={ticketsPath()}>Go to Tickets</Link>
+                    </Button>
+                }/>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <h2 className='text-lg'>{ticket.title}</h2>
+            <h2 className='text-sm'>{ticket.content}</h2>
+        </div>
+    )
 }
 
 export default TicketPage
---
+++ src/components/placeholder.tsx
@@ -0,0 +1,28 @@
+import React, {cloneElement} from "react";
+import {LucideMessageSquareWarning} from "lucide-react";
+
+type labelProps = {
+    label: string,
+    icon?: React.ReactElement,
+    button?: React.ReactElement
+}
+const Placeholder = ({
+                         label,
+                         icon = <LucideMessageSquareWarning/>,
+                         button = <div/>,
+                     }: labelProps) => {
+    return (
+        <div className="flex-1 self-center flex flex-col items-center justify-center gap-y-2">
+            {/*cloneElement用来覆盖原元素的尺寸*/}
+            {cloneElement(icon, {
+                className: 'w-16 h-16',
+            })}
+            <h2 className="text-lg text-center">{label}</h2>
+            {cloneElement(button, {
+                className: 'h-10',
+            })}
+        </div>
+    )
+}
+
+export {Placeholder}
