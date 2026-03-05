62a370b741cf7fe1d5eb2d5f729616f265a2e1f5
jasontan <j000102@sohu.com>
2026-03-05 17:46:11 +0800

    feat: fetchTickets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html suppressContentEditableWarning lang="en">
+        <html suppressHydrationWarning lang="en">
         <body
             className={inter.className}
         >
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,10 +1,23 @@
+"use client"
+
 import {initialTickets} from "@/data";
 import {Separator} from "@/components/ui/separator";
 import Heading from "@/components/heading";
 import {TicketItem} from "@/features/ticket/components/ticket-item";
-
+import {useEffect, useState} from "react";
+import {Ticket} from "@/features/types";
+import {getTicket} from "@/features/ticket/queries/get-ticket";
 
 const TicketsPage = () => {
+    const [tickets, setTickets] = useState<Ticket[]>([]);
+
+    const fetchTickets = async () => {
+        const result = await getTicket()
+        setTickets(result)
+    }
+    useEffect(() => {
+        fetchTickets()
+    }, []);
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets" description="All your tickets at one place"/>
@@ -12,7 +25,7 @@ const TicketsPage = () => {
             <Separator/>
             <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
                 {
-                    initialTickets.map((ticket) => (
+                    tickets.map((ticket) => (
                         <TicketItem ticket={ticket} key={ticket.id} isDetail={false}/>
                     ))
                 }
