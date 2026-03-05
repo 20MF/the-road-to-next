ce24b145604f11a4f6d67c2c33c9e0d30217ecaf
jasontan <j000102@sohu.com>
2026-03-05 22:13:29 +0800

    feat: add loading组件

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -6,6 +6,7 @@ import {TicketItem} from "@/features/ticket/components/ticket-item";
 import {getTickets} from "@/features/ticket/queries/get-tickets";
 import {Suspense} from "react";
 import {TicketList} from "@/features/ticket/components/ticket-list";
+import {Spinner} from "@/features/ticket/components/spinner";
 
 // 只有在服务器端组件中才能通过异步操作直接获取数据,客户端组件不能如此操作
 const TicketsPage =async () => {
@@ -13,9 +14,11 @@ const TicketsPage =async () => {
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets" description="All your tickets at one place"/>
 
-            <Suspense>
+            <Suspense fallback={<Spinner/>}>
                 <TicketList/>
             </Suspense>
+
+            {/*<Spinner/>*/}
         </div>
     )
 }
