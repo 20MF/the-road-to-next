5c4f39b95eaf20d38723cda9d529ed7b64a56d60
jasontan <j000102@sohu.com>
2026-03-05 21:59:39 +0800

    feat: Suspense缓存使用

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,31 +4,18 @@ import {Separator} from "@/components/ui/separator";
 import Heading from "@/components/heading";
 import {TicketItem} from "@/features/ticket/components/ticket-item";
 import {getTickets} from "@/features/ticket/queries/get-tickets";
+import {Suspense} from "react";
+import {TicketList} from "@/features/ticket/components/ticket-list";
 
 // 只有在服务器端组件中才能通过异步操作直接获取数据,客户端组件不能如此操作
 const TicketsPage =async () => {
-    const tickets=await getTickets()
-    // const [tickets, setTickets] = useState<Ticket[]>([]);
-    //
-    // const fetchTickets = async () => {
-    //     const result = await getTickets()
-    //     setTickets(result)
-    // }
-    // useEffect(() => {
-    //     fetchTickets()
-    // }, []);
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="Tickets" description="All your tickets at one place"/>
 
-            <Separator/>
-            <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
-                {
-                    tickets.map((ticket) => (
-                        <TicketItem ticket={ticket} key={ticket.id} isDetail={false}/>
-                    ))
-                }
-            </div>
+            <Suspense>
+                <TicketList/>
+            </Suspense>
         </div>
     )
 }
