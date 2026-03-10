632fddc22975dfb5d07163887b53c31ab7d3347a
jasontan <j000102@sohu.com>
2026-03-10 21:23:19 +0800

    feat: redirect

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,6 +1,8 @@
 "use server"
 
 import {prisma} from "@/lib/prisma";
+import {redirect} from "next/navigation";
+import {ticketsPath} from "@/paths";
 
 const deleteTicket = async (id: string) => {
     await prisma.ticket.delete({
@@ -8,6 +10,7 @@ const deleteTicket = async (id: string) => {
             id
         }
     })
+    redirect(ticketsPath())
 }
 
 export {deleteTicket}
