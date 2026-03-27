ba4901e65a8d9b21b2a99b0d12617968611ca194
jasontan <j000102@sohu.com>
2026-03-28 00:35:35 +0800

    feat: 增加防颤抖功能

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -2,14 +2,17 @@
 import {Input} from "@/components/ui/input";
 import React from "react";
 import {usePathname, useSearchParams, useRouter} from "next/navigation";
+import { useDebouncedCallback} from "use-debounce";
 
 export type SearchInputProps = {
     placeholder: string
 }
 
-// 1、获取初始查询状态字符串,赋值给URLSearchParams
-// 2、通过event事件,replace不断替换路由地址
-// 3、replace会传递给useSearchParams更新URL地址栏
+/*
+ 1、获取初始查询状态字符串,赋值给URLSearchParams
+ 2、通过event事件,replace不断替换路由地址
+ 3、replace会传递给useSearchParams更新URL地址栏
+ */
 
 const SearchInput = ({placeholder}: SearchInputProps) => {
     //获取URL当前查询值,管理URL地址栏状态更新
@@ -19,7 +22,7 @@ const SearchInput = ({placeholder}: SearchInputProps) => {
 
     // console.log("params", searchParams.get("search"))
 
-    const handeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handeSearch = useDebouncedCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         const value = event.target.value
         //赋予
         const params = new URLSearchParams(searchParams)
@@ -37,7 +40,7 @@ const SearchInput = ({placeholder}: SearchInputProps) => {
             scroll: false
         })
 
-    }
+    }, 250)
 
     return (
         <Input placeholder={placeholder} onChange={handeSearch}/>
