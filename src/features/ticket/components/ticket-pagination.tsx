a7bd20a8885a93e2f1b7041c6da9d1150727451f
jasontan <j000102@sohu.com>
2026-03-30 22:34:06 +0800

    feat: 查询结果后,重新归零分ˆ页

--- src/features/ticket/components/ticket-pagination.tsx
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -1,7 +1,9 @@
 "use client"
-import {useQueryStates} from "nuqs";
-import {paginationOption, paginationParser} from "@/features/ticket/search-params";
+import {useQueryState, useQueryStates} from "nuqs";
+import {paginationOption, paginationParser, searchParser} from "@/features/ticket/search-params";
 import {Pagination} from "@/components/pagination";
+import {useEffect, useRef} from "react";
+import {search} from "effect/String";
 
 type TicketPaginationProps = {
     paginatedTicketMetadata: {
@@ -12,6 +14,17 @@ type TicketPaginationProps = {
 
 const TicketPagination = ({paginatedTicketMetadata}: TicketPaginationProps) => {
     const [pagination, setPagination] = useQueryStates(paginationParser, paginationOption)
+
+    const [search] = useQueryState("search", searchParser)
+    const prevSearch = useRef(search)
+
+    //每当查询结果后,重新归零分页
+    useEffect(() => {
+        if (search === prevSearch.current) return
+        prevSearch.current = search
+        setPagination({...pagination, page: 0})
+
+    }, [search, pagination, setPagination]);
 
     return <Pagination
         pagination={pagination}
